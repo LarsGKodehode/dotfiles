ab59bcc09a723a96afdcb7017dbf3b507e02c37b
zabronax <zabronax@gmail.com>
2024-07-06 15:26:44 +0000

    minimal system definition

---
+++ configuration.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = with pkgs; [
+    git
+    curl
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "24.05"; # Don't touch!
+
+            wsl = {
+              enable = true;
+              defaultUser = "zab";
+            };
+          }
+
+        ];
+      };
+    };
+  };
+}
