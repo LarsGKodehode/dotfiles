5cc59d3158c598212ab696e13eb5e2f2eae4a3cd
zabronax <zabronax@gmail.com>
2024-07-15 21:22:55 +0000

    fix color format error

--- colorscheme/gruvbox-light-soft/default.nix
+++ colorscheme/gruvbox-light-soft/default.nix
@@ -2,21 +2,21 @@
   name = "Gruvbox light, soft";
   author = "Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)";
   light = {
-    base00 = "f2e5bc"; # ----
-    base01 = "ebdbb2"; # ---
-    base02 = "d5c4a1"; # --
-    base03 = "bdae93"; # -
-    base04 = "665c54"; # +
-    base05 = "504945"; # ++
-    base06 = "3c3836"; # +++
-    base07 = "282828"; # ++++
-    base08 = "9d0006"; # red
-    base09 = "af3a03"; # orange
-    base0A = "b57614"; # yellow
-    base0B = "79740e"; # green
-    base0C = "427b58"; # aqua/cyan
-    base0D = "076678"; # blue
-    base0E = "8f3f71"; # purple
-    base0F = "d65d0e"; # brown
+    base00 = "#F2E5BC"; # ----
+    base01 = "#EBdBB2"; # ---
+    base02 = "#D5C4A1"; # --
+    base03 = "#BDAE93"; # -
+    base04 = "#665C54"; # +
+    base05 = "#504945"; # ++
+    base06 = "#3C3836"; # +++
+    base07 = "#282828"; # ++++
+    base08 = "#9D0006"; # red
+    base09 = "#AF3A03"; # orange
+    base0A = "#B57614"; # yellow
+    base0B = "#79740E"; # green
+    base0C = "#427B58"; # aqua/cyan
+    base0D = "#076678"; # blue
+    base0E = "#8F3F71"; # purple
+    base0F = "#D65D0E"; # brown
   };
 }
